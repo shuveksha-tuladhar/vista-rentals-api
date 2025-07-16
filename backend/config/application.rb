d0cc0939c22335a19a63a533e62d53d7812abc7d
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-07-16 14:27:49 -0700

    Added cors to allow frontend api call

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -38,5 +38,15 @@ module VistaRentals
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins ENV.fetch("CORS_ORIGIN", "*")
+
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
