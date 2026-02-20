2b4de2c44ef498a6f9e86c0ede98d212e44f278c
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2026-02-19 23:28:31 -0800

    add host listings typescript types

---
+++ frontend/src/pages/HostListings/types.ts
@@ -0,0 +1,34 @@
+export interface HostListingBedInfo {
+  room: string;
+  bed_type: string;
+}
+
+export interface HostListingImage {
+  url: string;
+}
+
+export interface HostListing {
+  id: number;
+  title: string;
+  name: string;
+  description: string;
+  property_type: string;
+  price: string;
+  city: string;
+  state: string;
+  country: string;
+  bedrooms: number;
+  baths: number;
+  max_guests: number;
+  property_images: HostListingImage[];
+  amenities: { id: number; name: string }[];
+  property_bed_infos: HostListingBedInfo[];
+}
+
+export interface EditListingFormValues {
+  title: string;
+  description: string;
+  price: string;
+  photos: string[];
+  amenity_names: string[];
+}
